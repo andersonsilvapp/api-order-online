133163da2ba8371e5f182bde2ed9366d1160fe8c
Anderson Silva <andersonsilvapp@gmail.com>
2021-09-18 17:20:49 -0300

    feat: update method implemented

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -30,7 +30,18 @@ class CategoryController {
     response.json(category);
   }
 
-  async update(request, response) {}
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, { name });
+
+    response.json(category);
+  }
 
   async delete(request, response) {}
 }
