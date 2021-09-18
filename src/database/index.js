3e939761966b33253fe5cead2513062d2e898e0b
Anderson Silva <andersonsilvapp@gmail.com>
2021-09-18 17:25:21 -0300

    db configs updated

--- src/database/index.js
+++ src/database/index.js
@@ -5,7 +5,7 @@ const client = new Client({
   port: 5432,
   user: 'root',
   password: 'root',
-  database: 'orderOnline',
+  database: 'orderonline',
 });
 
 client.connect();
--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,1 +1,18 @@
 CREATE DATABASE orderOnline;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  description VARCHAR,
+  ingredients VARCHAR,
+  additional VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id)
+);
