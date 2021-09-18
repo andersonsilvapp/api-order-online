9d114ae95a5250af1ebfb532dc59b56056534550
Anderson Silva <andersonsilvapp@gmail.com>
2021-09-18 17:20:37 -0300

    feat: store method implemented

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -18,7 +18,17 @@ class CategoryController {
     response.json(category);
   }
 
-  async store(request, response) {}
+  async store(request, response) {
+    const { name } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.create({ name });
+
+    response.json(category);
+  }
 
   async update(request, response) {}
 
