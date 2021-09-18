aae2fd2b3c011e96ed155ca5a5404882cf90d976
Anderson Silva <andersonsilvapp@gmail.com>
2021-09-18 17:22:59 -0300

    feat: findById(repository) method implemented

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -10,7 +10,15 @@ class CategoriesRepository {
     return rows;
   }
 
-  async findById(id) {}
+  async findById(id) {
+    const [row] = await db.query(`
+      SELECT *
+      FROM categories
+      WHERE id=$1
+    `, [id]);
+
+    return row;
+  }
 
   async create({ name }) {}
 
