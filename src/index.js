1c06fa9221e3179326b65c144adbd5dd7e50d95e
Anderson Silva <andersonsilvapp@gmail.com>
2021-09-28 03:34:55 -0300

    refactor: cors rules corrected

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(5000, () => console.log('Server started at http://localhost:5000'));
